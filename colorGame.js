246390f271cccb969c0c9f5a746044163636eedf
Esteban Gutierrez <estebangutierrez@estebans-air.attlocal.net>
2019-08-14 09:41:18 -0700

    click event added

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,25 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll('.square');
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById('colorDisplay');
+
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i<squares.length; i++){
+    //add initial colors to squres
     squares[i].style.backgroundColor = colors[i];
+    //add clikc listeners to squares
+    squares[i].addEventListener('click', function(){
+    //grab color of clicked squares
+    var clickedColor = this.style.backgroundColor;
+    //compare to pickedColor
+    if(clickedColor === pickedColor){
+        alert('Correct'); 
+    } else{
+        alert('Wrong')
+    }
+    
+
+    })
 }
