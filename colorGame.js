1837ef79c504d09bae00e5013c568e25a5d2d40e
Esteban Gutierrez <estebangutierrez@estebans-air.attlocal.net>
2019-08-15 09:04:59 -0700

    added random pick of color, display error or correct pick

--- colorGame.js
+++ colorGame.js
@@ -8,9 +8,9 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
-
+var messageDisplay = document.querySelector('#message');
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i<squares.length; i++){
@@ -22,11 +22,24 @@ for(var i = 0; i<squares.length; i++){
     var clickedColor = this.style.backgroundColor;
     //compare to pickedColor
     if(clickedColor === pickedColor){
-        alert('Correct'); 
+        messageDisplay.textContent = 'Correct!' 
+        changeColors(clickedColor);
     } else{
-        alert('Wrong')
+        this.style.backgroundColor = '#232323';
+        messageDisplay.textContent = 'Try Again!!'
     }
-    
+  })
+}
 
-    })
+function changeColors(color){
+    //loop trhough all squares
+    for(var i = 0; i < squares.length; i++){
+        //change each color to macth given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
